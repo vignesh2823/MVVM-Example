568842455b17a3e6d32e2561ff01a45e01f2f455
vignesh2823 <vigneshusasv@gmail.com>
2021-04-30 11:39:26 +0530

    Toolbar ui changes,String resource changes

--- app/src/main/java/com/example/myapplication/Fragment/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/Fragment/HomeFragment.kt
@@ -1,14 +1,17 @@
 package com.example.mvvmtask.Fragment
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Color
 import android.net.ConnectivityManager
 import android.net.NetworkInfo
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuInflater
+import android.util.Log
 import android.view.View
-import androidx.appcompat.widget.SearchView
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -23,6 +26,7 @@ import com.example.mvvmtask.Util.Status
 import com.example.mvvmtask.Util.ViewModelFactory
 import com.example.mvvmtask.ViewModel.HomeViewModel
 import com.example.myapplication.R
+import com.google.android.material.appbar.AppBarLayout
 import kotlinx.android.synthetic.main.fragment_home.*
 
 
@@ -30,8 +34,54 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     private lateinit var viewModel: HomeViewModel
     lateinit var adapter: HomeAdapter
 
+    private val TAG = "ToolbarFragment"
+    private val STANDARD_APPBAR = 0
+    private val SEARCH_APPBAR = 1
+    private var mAppBarState: Int = 0
+    lateinit var viewContactsBar: AppBarLayout
+    lateinit var searchBar: AppBarLayout
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewContactsBar = view.findViewById(R.id.viewContactsToolbar) as AppBarLayout
+        searchBar = view.findViewById(R.id.searchToolbar) as AppBarLayout
+
+        setAppBaeState(STANDARD_APPBAR);
+
+        val ivSearchContact = view.findViewById(R.id.ivSearchIcon) as ImageView
+        val ivBackArrow = view.findViewById(R.id.ivBackArrow) as ImageView
+        val ivClose = view.findViewById(R.id.ivClose) as ImageView
+        val etSearch = view.findViewById(R.id.etSearch) as EditText
+
+        ivSearchContact.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View) {
+                Log.d(TAG, "onClick: clicked searched icon")
+                toggleToolBarState()
+                etSearch.requestFocus()
+            }
+        })
+
+        ivBackArrow.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View) {
+                Log.d(TAG, "onClick: clicked back icon")
+                toggleToolBarState()
+                etSearch.clearFocus()
+            }
+        })
+
+        ivClose.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View) {
+                Log.d(TAG, "onClick: clicked back icon")
+                toggleToolBarState()
+                etSearch.text = null
+                etSearch.clearFocus()
+            }
+        })
+        etSearch.addTextChangedListener {
+            adapter.filter.filter(etSearch.text.trim())
+        }
+
         setupViewModel()
         swipe.setOnRefreshListener {
             swipe.setColorSchemeColors(Color.WHITE)
@@ -137,25 +187,60 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         list_view.adapter = adapter
     }
 
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.menu_search, menu)
-        super.onCreateOptionsMenu(menu, inflater)
+    /*
+        override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+            inflater.inflate(R.menu.menu_search, menu)
+            super.onCreateOptionsMenu(menu, inflater)
 
-        val searchItem = menu.findItem(R.id.action_search)
-        val searchView = searchItem.actionView as SearchView
+            val searchItem = menu.findItem(R.id.action_search)
+            val searchView = searchItem.actionView as SearchView
 
-        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                return true
-            }
-
-            override fun onQueryTextChange(newText: String?): Boolean {
-                if (newText != null) {
-                    adapter.filter.filter(newText)
+            searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    return true
                 }
-                return true
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    if (newText != null) {
+                        adapter.filter.filter(newText)
+                    }
+                    return true
+                }
+            })
+        }
+    */
+
+    @SuppressLint("ServiceCast")
+    private fun setAppBaeState(state: Int) {
+        Log.d(TAG, "setAppBaeState: changing app bar state to: " + state)
+        mAppBarState = state
+        if (mAppBarState === STANDARD_APPBAR) {
+            searchBar.setVisibility(View.GONE)
+            viewContactsBar.setVisibility(View.VISIBLE)
+            val view = getView()
+            val im =
+                requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            try {
+                im.hideSoftInputFromWindow(view?.getWindowToken(), 0) // make keyboard hide
+            } catch (e: NullPointerException) {
+                Log.d(TAG, "setAppBaeState: NullPointerException: " + e)
             }
-        })
+        } else if (mAppBarState === SEARCH_APPBAR) {
+            viewContactsBar.setVisibility(View.GONE)
+            searchBar.setVisibility(View.VISIBLE)
+            val im =
+                getActivity()?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            im.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0) // make keyboard popup
+        }
+    }
+
+    private fun toggleToolBarState() {
+        Log.d(TAG, "toggleToolBarState: toggling AppBarState.")
+        if (mAppBarState === STANDARD_APPBAR) {
+            setAppBaeState(SEARCH_APPBAR)
+        } else {
+            setAppBaeState(STANDARD_APPBAR)
+        }
     }
 
     private fun isNetworkAvailable(): Boolean {
